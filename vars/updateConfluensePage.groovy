37eeb674f9e335b565fc48e98f05a7dbbd30e351
Mangesh <Mangeshbangale27@gmail.com>
2022-12-30 17:00:06 +0530

    a

--- vars/updateConfluensePage.groovy
+++ vars/updateConfluensePage.groovy
@@ -37,7 +37,7 @@ withCredentials([
         def credentials = USERNAME + ":" + PASSWORD
         println(USERNAME)
         println(PASSWORD)
-        siteConnection.setRequestProperty("Authorization", "Basic " + /*new String(Base64.getEncoder().encode(credentials.getBytes()*/credentials )))
+        siteConnection.setRequestProperty("Authorization", "Basic " + credentials ) //new String(Base64.getEncoder().encode(credentials.getBytes()))
         def responseCode = siteConnection.getResponseCode()
         println(responseCode)
 
