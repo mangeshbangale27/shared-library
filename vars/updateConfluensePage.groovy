e2a076fa8abcc60a7ecb26a7b11eccd171099f2e
mangeshbangale27 <49510221+mangeshbangale27@users.noreply.github.com>
2023-01-05 19:21:12 +0530

    Update updateConfluensePage.groovy

--- vars/updateConfluensePage.groovy
+++ vars/updateConfluensePage.groovy
@@ -68,6 +68,7 @@ withCredentials([
         siteConnection.getOutputStream().write(new JsonBuilder(jsonData).toPrettyString().getBytes("UTF-8"))
         statusUpdate = siteConnection.getResponseCode();
         println("page updated successfully...")
+        println(DEPLOY_TIME)
 
         if (statusUpdate != 200) {
             sleep(1000)  // 1 sec delay - may be another process try to update confluense page
