625fa2c4715923e58c354e76dd5fad77932598d0
mangeshbangale27 <49510221+mangeshbangale27@users.noreply.github.com>
2023-01-18 21:53:49 +0530

    Update updateConfluensePage.groovy

--- vars/updateConfluensePage.groovy
+++ vars/updateConfluensePage.groovy
@@ -64,8 +64,8 @@ withCredentials([
 // def newRow = "<td><p><strong>${ENVIRONMET}</strong></p></td><td><p>${STATUS}</p></td><td><p>${SOURCE_BRANCH}</p></td><td><p>${IMAGE_TAG}</p></td><td><p>${DEPLOY_TIME}</p></td>"
 
 // jsonData.body.storage.value << newRow
-        println(jsonData.body.storage.value.table)
-        jsonData.body.storage.value + = "<td><p><strong>PROD</strong></p></td><td><p>STATUS</p></td><td><p>SOURCE_BRANCH</p></td><td><p>IMAGE_TAG</p></td><td><p>DEPLOY_TIME</p></td>"
+        println(jsonData.body.storage.value[0])
+       
         jsonData.body.storage.value = jsonData.body.storage.value.replaceFirst("<td><p><strong>${ENVIRONMET}</strong></p></td><td><p>(.*?)</p></td><td><p>(.*?)</p></td><td><p>(.*?)</p></td><td><p>(.*?)</p></td>","<td><p><strong>${ENVIRONMET}</strong></p></td><td><p>${STATUS}</p></td><td><p>${SOURCE_BRANCH}</p></td><td><p>${IMAGE_TAG}</p></td><td><p>${DEPLOY_TIME}</p></td>")
         //jsonData.body.storage.value = jsonData.body.storage.value.insert("<td><p><strong>${ENVIRONMET}</strong></p></td><td><p>${STATUS}</p></td><td><p>${SOURCE_BRANCH}</p></td><td><p>${IMAGE_TAG}</p></td><td><p>${DEPLOY_TIME}</p></td>")
         
