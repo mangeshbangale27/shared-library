5b99bf8d2a52cbfcd0c47d6aaf41f9dc1cbe45bc
mangeshbangale27 <49510221+mangeshbangale27@users.noreply.github.com>
2023-01-19 11:34:06 +0530

    Update testconfluence.groovy

--- vars/testconfluence.groovy
+++ vars/testconfluence.groovy
@@ -1,40 +1,27 @@
 @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7')
+
 import groovyx.net.http.HTTPBuilder
-import groovy.json.JsonSlurper
 
-def confluenceURL = "https://mangeshbangale.atlassian.net"
-def accessToken = "excKBCr7rFgrerpnFIsC6888"
-def spaceKey = "MANGESH"
-def pageTitle = "demo"
+def http = new HTTPBuilder("https://mangeshbangale.atlassian.net")
+def auth = 'Basic ' + "mangeshbangale27@gmail.com:excKBCr7rFgrerpnFIsC6888".bytes.encodeBase64().toString()
 
-def http = new HTTPBuilder(confluenceURL)
-
-// Get page ID
-def pageIdResponse = http.get(path: "/rest/api/content", headers: [
-    'Content-Type': 'application/json',
-    'Authorization': "Bearer ${accessToken}"
-], query: [
-    title: pageTitle,
-    spaceKey: spaceKey
-])
-def pageId = new JsonSlurper().parseText(pageIdResponse.getData()).results[0].id
-
-// Update page with new line
-def updateResponse = http.put(path: "/rest/api/content/${pageId}", headers: [
-    'Content-Type': 'application/json',
-    'Authorization': "Bearer ${accessToken}"
-], json: [
-    body: [
-        storage: [
-            value: '<p>Line 1</p>', // replace "Line 1" with the actual line you want to add
-            representation: 'storage'
+http.request(Method.PUT, ContentType.JSON) { req ->
+    headers.'Authorization' = auth
+    body = [
+        id: pageId,
+        type: 'page',
+        title: 'New Page Title',
+        body: [
+            storage: [
+                value: 'New page content',
+                representation: 'storage'
+            ]
         ]
     ]
-])
-
-if (updateResponse.status == 200) {
-    println 'Line added successfully'
-} else {
-    println 'Error adding line: ' + updateResponse.status
+    response.success = { resp, json ->
+        println 'Page Updated Successfully'
+    }
+    response.failure = { resp ->
+        println 'Page Update Failed'
+    }
 }
-
